9b73c7979431c25abcf7fa8debf6a2b275d514c3
Stew Eucen <12193670+StewEucen@users.noreply.github.com>
2026-08-27 12:14:09 +0900

    Fix allowedAuthors option of jsdoc/check-values in rules/core.js

--- rules/core.js
+++ rules/core.js
@@ -105,7 +105,7 @@ export default {
     'jsdoc/check-values': [
       'error',
       {
-        allowedAuthors: [],
+        // allowedAuthors: null, // The schema accepts an array only
         allowedLicenses: [],
         numericOnlyVariation: false,
         licensePattern: '/([^\\\\n\\\\r]*)/gu',
